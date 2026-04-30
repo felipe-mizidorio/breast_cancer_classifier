23f87c2d481392afbc3b4b35db36b2441c449d84
felipe-mizidorio <felipemerenda1@gmail.com>
2026-04-30 12:31:07 +0100

    feat: add logging configuration to be used for all src packages

--- src/breast_cancer_classifier/__init__.py
+++ src/breast_cancer_classifier/__init__.py
@@ -1,2 +1,8 @@
-def main() -> None:
-    print("Hello from breast-cancer-classifier!")
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    handlers=[logging.StreamHandler()],
+)
